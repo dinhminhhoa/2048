5598d793c0c94c26580b19e8c8738db059a5a3ea
DESKTOP-83LEQP4\Admin <dinhminhhoa25061999@gmail.com>
2024-03-21 19:43:47 +0700

    [add] done

---
+++ Assets/Script/TileState.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Tile")]
+public class TileState : ScriptableObject
+{
+    public int number;
+    public Color backgroundColor;
+    public Color textColor;
+    
+}
